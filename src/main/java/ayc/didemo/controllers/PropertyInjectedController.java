5d2823b2dbf235c24fac1e8259aa10be6bd4bd75
Ayce Ciloglugil <ayceciloglugil@gmail.com>
2018-11-29 14:59:30 +0300

    Primary bean overrides even if bean named is used so adding qualifier

--- src/main/java/ayc/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/ayc/didemo/controllers/PropertyInjectedController.java
@@ -1,6 +1,7 @@
 package ayc.didemo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import ayc.didemo.services.GreetingService;
@@ -8,6 +9,7 @@ import ayc.didemo.services.GreetingService;
 @Controller
 public class PropertyInjectedController {
 	@Autowired
+	@Qualifier("greetingServiceImpl")
 	public GreetingService greetingServiceImpl;
 
 	public String sayHello() {
